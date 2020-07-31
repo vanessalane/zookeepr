3f0ae9943ca592865ed1ed89272c3b7de370fe59
Vanessa Lane <vlane@23andme.com>
2020-07-30 23:03:47 -0700

    handle animal creation

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -32,7 +38,33 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-  }
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -48,6 +80,19 @@ app.get('/api/animals/:id', (req, res) => {
         res.json(result);
     } else {
         res.sendStatus(404);
+    }
+});
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // add animal to json file and animals array in this function
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
 });
 
